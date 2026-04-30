d7f398a8cfc837f6c57def1fa47f8d03acde856e
Loren nunez <lorensofian23@gmail.com>
2026-04-30 18:54:42 -0500

    se agrego menu de opciones

--- proyecto-fotografico.py
+++ proyecto-fotografico.py
@@ -1,2 +1,11 @@
-// Proyecto PhotoCampus
-// Aquí se implementarán las funciones
+opciones=int(input("ingrese la opcion que desea:\n 1.Registrar servivio.\n 2.Agregar nuevos servicios.\n 3.Eliminar servicios"))
+match opciones:
+    case 1:
+        if opciones== 1:
+            print()
+    case 2:
+        if opciones== 2:
+            print()
+    case 3:
+        if opciones== 3:
+            print()
