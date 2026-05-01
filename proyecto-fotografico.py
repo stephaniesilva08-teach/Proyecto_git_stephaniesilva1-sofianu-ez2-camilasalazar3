e53faa733ff39a669cb8282a15368ee8fbf1a6d1
Loren nunez <lorensofian23@gmail.com>
2026-04-30 20:37:50 -0500

    feat:cambiamos la funcion de archivo

--- proyecto-fotografico.py
+++ proyecto-fotografico.py
@@ -1,23 +1,2 @@
-import json
-def editar_servicios():
-    with open("photo-campus.json", "r") as archivo:
-        servicios_fotografia=json.load(archivo)
-    
-    print("Editar servicios")
-    codigo=int(input("ingrese el codigo del servicio que desea editar:"))
-    codigo_encontrado=False
-    
-    for servicio in servicios_fotografia:
-            if servicio["codigo"] == codigo:
-                print("servicio encontrado")
-                servicio["duracion"] = input("duracion:")
-                servicio["precio"] = float(input("Nuevo precio:"))
-                codigo_encontrado=True
-                print("su servicio fue actualizado correctamente ")
-                break
-    if not codigo_encontrado:
-        print("no se encontro el servicio amiguibi")
-    
-    with open("photo-campus.json", "w") as archivo:
-        json.dump(servicios_fotografia,archivo)
+
     
