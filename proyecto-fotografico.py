789a5e9550b289fff13373f2eabad02ca9a836ae
Loren nunez <lorensofian23@gmail.com>
2026-04-30 19:02:41 -0500

    agregue menu principal

--- proyecto-fotografico.py
+++ proyecto-fotografico.py
@@ -2,10 +2,10 @@ opciones=int(input("ingrese la opcion que desea:\n 1.Registrar servivio.\n 2.Agr
 match opciones:
     case 1:
         if opciones== 1:
-            print()
+            print()#funcion agregar
     case 2:
         if opciones== 2:
-            print()
+            print()#funcion editar
     case 3:
         if opciones== 3:
-            print()
+            print()#funcion eliminar
